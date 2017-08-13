36a7d232b4788f9739ca81ed1d47429755b256ce
Racheal Alake <Racheal Alake>
2017-08-13 15:41:33 +0100

    app config first attempt

--- app.js
+++ app.js
@@ -1,6 +1,66 @@
 var app = angular.module("mochunksApp", []);
 
-// app.config()
+app.config($routeProvider, $locationProvider) {
+
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/landingPage/landingpage.view.html', //
+            controller: 'landingPageCtrl',
+            controllerAs: 'vm',
+        })
+        .when('/dashboard', {
+            templateUrl: 'pages/articlePage/articlePage.view.html',
+            controller: 'dashboardCtrl',
+            controllerAs: 'vm'
+        })
+        .when('/resultpage', {
+            templateUrl: 'pages/resultPage/resultpage.view.html',
+            controller: 'resultPageSingleURL',
+            controllerAs: 'vm'
+        })
+        .when('/eventpage', {
+            template: '<event-page></event-page>',
+        })
+        .when('/about', {
+            templateUrl: 'pages/aboutPage/about.view.html',
+            controller: 'aboutCtrl',
+            controllerAs: 'vm'
+        })
+        .when('/accountSettings', {
+            templateUrl: 'pages/accountSettings/accountsetting.view.html',
+            controller: 'accountSettingCtrl',
+            controllerAs: 'vm'
+        })
+        .when('/privacyUrl', {
+            templateUrl: 'pages/privacyUrlPage/privacyUrl.view.html'
+        })
+        .when('/alpha', {
+            templateUrl: 'pages/alphaTestPage.view.html'
+        })
+        .when('/admin', {
+            templateUrl: 'pages/adminPage/admin.view.html',
+            controller: 'adminCtrl',
+            controllerAs: 'Admin'
+        })
+        .when('/adminEditEvent', {
+            templateUrl: 'pages/adminViewEventPage/adminViewEventPage.view.html',
+            controller: 'adminViewEventPageCtrl',
+            controllerAs: 'Admin'
+        })
+        .when('/forgotPassword', {
+            template: '<forgot-password></forgot-password>'
+        })
+        .when('/reset-password/:verification_link', {
+            template: '<reset-password></reset-password>'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+    $locationProvider.html5Mode({
+        requireBase: false,
+        enabled: true
+    });
+}
 
 
 app.controller("mainCtrl", function() {

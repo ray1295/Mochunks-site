e8030404785cd3318009cc7de3cee97a1176d6da
ray1295 <ralake95@gmail.com>
2017-08-28 15:42:53 +0100

    pushing for richmond

--- app_client/pages/chitChatPage/chitChatPage.controller.js
+++ app_client/pages/chitChatPage/chitChatPage.controller.js
@@ -3,18 +3,15 @@ var app = angular.module('mochunksApp');
 app.controller('ChitChatController', function(ArticlesService){
 	var vm = this;
 	
-	ArticlesService.getAllArticles().then(function(response) {
-		console.log(response)
-	}).catch(function(err) {
-		console.log(err);
-	});
 	ArticlesService.getArticlesBySection('chitchat').then(function(response){
 		vm.articles = response.data;
-		console.log(vm.articles);
 	}).catch(function(err){
 		alert("Could not load articles");
 		console.log("Could not load articles");
 	});
+	vm.fullArticle = function() {
+		
+	};
 });
 
 

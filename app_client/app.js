bdd550bdf0c622fee310746aadfc446ef82ddcf2
ray1295 <ralake95@gmail.com>
2017-09-01 21:53:16 +0100

    for backend

--- app_client/app.js
+++ app_client/app.js
@@ -69,7 +69,29 @@ var app = angular.module("mochunksApp", ['ngRoute', 'ngAnimate', 'ngSanitize', '
 				redirectTo: '/'
 			});
 	});
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop >= 600 || document.documentElement.scrollTop >= 600) {
+		document.getElementById("go-top").style.display = "block";
+	}
+	else {
+		document.getElementById("go-top").style.display = "none";
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+vm.topFunction = function() {
+	document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+	document.documentElement.scrollTop = 0; // For IE and Firefox
+
+};
 
 app.controller("mainCtrl", function () {
 	var vm = this;
 });
+
+

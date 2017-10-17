c97080794d3ae634d34098acf72afea9dd48d12c
Richmond <richmond.alake@gmail.com>
2017-10-17 02:44:34 +0100

    change the user relationship to fix error

--- app_api/models/User.js
+++ app_api/models/User.js
@@ -8,11 +8,11 @@ var Types = keystone.Field.Types;
 var User = new keystone.List('User');
 
 User.add({
-	name: { type: Types.Name, required: true, index: true },
-	email: { type: Types.Email, initial: true, required: true, unique: true, index: true },
-	password: { type: Types.Password, initial: true, required: true },
+	name: {type: Types.Name, required: true, index: true},
+	email: {type: Types.Email, initial: true, required: true, unique: true, index: true},
+	password: {type: Types.Password, initial: true, required: true},
 }, 'Permissions', {
-	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
+	isAdmin: {type: Boolean, label: 'Can access Keystone', index: true},
 });
 
 // Provide access to Keystone
@@ -20,12 +20,10 @@ User.schema.virtual('canAccessKeystone').get(function () {
 	return this.isAdmin;
 });
 
-
 /**
  * Relationships
  */
-User.relationship({ ref: 'Post', path: 'posts', refPath: 'author' });
-
+User.relationship({ref: 'User', path: 'users', refPath: 'user'});
 
 /**
  * Registration

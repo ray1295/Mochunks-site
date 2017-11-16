6929733e03df7b15e4c3bd75e4bdb39dd605b973
Richmond <richmond.alake@gmail.com>
2017-11-16 23:43:32 +0000

    updated ga

--- app_client/pages/thisWeekInMusic/thisWeekInMusic.controller.js
+++ app_client/pages/thisWeekInMusic/thisWeekInMusic.controller.js
@@ -3,11 +3,9 @@ var app = angular.module('wevativeApp');
 app.controller('ThisWeekInMusicController', function ($rootScope) {
 	var vm = this;
 
+	vm.articleSection = 'weekmusic';
+
 	// Hide Navigation bar on mobile once page is loaded
 	// TODO: Look for efficient solution to close the navigation bar on each page
 	$rootScope.showMenu = false;
-
-	vm.articleSection = 'weekmusic';
-
-
 });
